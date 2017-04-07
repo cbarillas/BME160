6a0d4534e16cfd661b315a5366e08eae5fd32167
cbarilla@ucsc.edu <cbarilla@unix3.lt.ucsc.edu>
2017-04-07 15:03:37 -0700

    minor change

--- lab01/helloAgain.py
+++ lab01/helloAgain.py
@@ -5,8 +5,8 @@
  
 """
 This is a demonstration program that shows off some basic interactive features
-of a python program. The program asks the user to enter a name, builds an object
-with that name, and then has the new object introduce itself.
+of a python program. The program asks the user to enter a name and their fav pet, builds an object
+with that name and pet, and then has the new object introduce itself.
  
 input: a string of arbitrary length, which is used to name the new person object
 output: greeting printed to screen
@@ -21,8 +21,8 @@ class Person:
         print("Hi there, I am {0} and I like {1}".format(self.myName,self.myPet))
 
 
-name = input( "What is my name? : " ) 
-pet = input( "What is my favorite kind of pet? : ")
+name = input("What is your name? : ") 
+pet = input("What is your favorite kind of pet? : ")
 
 newPerson = Person(name, pet)
 newPerson.introduce()
