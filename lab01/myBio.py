7a29ef39b349bb0daa4084f838c95d31901a1a2c
cbarilla@ucsc.edu <cbarilla@unix3.lt.ucsc.edu>
2017-04-07 22:34:50 -0700

    more comments

--- lab01/myBio.py
+++ lab01/myBio.py
@@ -8,22 +8,24 @@ Program that prints out information about myself
 """
  
 class Person:
+    """Iniatializes new human object of Person class with information about themselves"""
     def __init__(self,name,username,status,major,why,interest,experience):
-        self.myName = name
-        self.myUser = username
-        self.myStat = status
-        self.myMajor = major
+        self.name = name
+        self.username = username
+        self.status = status
+        self.major = major
         self.why = why
-        self.intrst = interest
-        self.exp = experience
+        self.interest = interest
+        self.experience = experience
         
     def introduce(self):
-        print("Hi there, I am {0}\nMy username is {1}".format(self.myName,self.myUser))
-        print("I am a {0}\nMy major is {1}".format(self.myStat,self.myMajor))
-        print("I'm hoping to {0}\nI'm {1}".format(self.why,self.intrst))
-        print("I have prior programming experience using {0}".format(self.exp))
+        """Print an introduction message based on info provided"""
+        print("Hi there, I am {0}\nMy username is {1}".format(self.name,self.username))
+        print("I am a {0}\nMy major is {1}".format(self.status,self.major))
+        print("I'm hoping to {0}\nI'm {1}".format(self.why,self.interest))
+        print("I have prior programming experience using {0}".format(self.experience))
  
-# constant names not part of Person class 
+"""Constant names not part of Person class""" 
 name = "Carlos Barillas"
 username = "cbarilla"
 status = "undergraduate"
@@ -32,6 +34,6 @@ why = "learn Python and how it can be applied to molecular biology"
 interest = "interested in gene therapy"
 experience = "Assembly, C, Java" 
 
-#create object called newPerson with my const data as paramters and print to stdout
+"""Create object called newPerson with my constant data as paramters and print to stdout"""
 newPerson = Person(name,username,status,major,why,interest,experience)
 newPerson.introduce()
