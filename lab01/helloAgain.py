5cee160a1226a0ceed83b0cf3b3d1aac87b89308
cbarilla@ucsc.edu <cbarilla@unix3.lt.ucsc.edu>
2017-04-07 22:09:05 -0700

    added docstrings to helloAgain

--- lab01/helloAgain.py
+++ lab01/helloAgain.py
@@ -12,16 +12,26 @@ output: greeting printed to screen
 """
  
 class Person:
+    """
+    Model simple humans that have a name and favorite pet
+
+    Keyword arguments:
+    name -- the name that the human wants to be known by
+    pet -- the humans favorite pet
+    """
     def __init__(self,name,pet):
+        """Capture the name and pet"""
         self.myName = name
         self.myPet = pet
 
     def introduce(self):
+        """Print an introuction message"""
         print("Hi there, I am {0} and I like {1}".format(self.myName,self.myPet))
 
-
+"""
+Build a new Person object, then tell it to introduce itself and their favorite pet
+"""
 name = input("What is your name? : ") 
 pet = input("What is your favorite pet? : ")
-
-newPerson = Person(name, pet)
+newPerson = Person(name,pet)
 newPerson.introduce()
