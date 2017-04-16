8e7cc7e29ef27c5db3179304446cb68cc31f8dd0
cbarilla@ucsc.edu <cbarilla@unix1.lt.ucsc.edu>
2017-04-15 22:35:55 -0700

    modified my removeN method and getN method

--- lab02/seqCleaner.py
+++ lab02/seqCleaner.py
@@ -2,30 +2,31 @@
 # Name: Carlos Barillas (cbarilla)
 # Group Members: none
 
-class SequenceClean(str):
+class SequenceCleaner(str):
     """
-    Sequence class returns a string object in upper case letters
+    SequenceCleaner class returns a string object in upper case letters
     Keyword arguments:
     sequence -- DNA sequence user enters
     """
-    def __new__(self,sequence):
+    def __new__(self, sequence):
         """Returns a copy of sequence in upper case letters"""
-        return str.__new__(self,sequence.upper())
+        return str.__new__(self, sequence.upper())
 
     def getN(self):
         """Returns the number of N's in the sequence"""
         num_N = self.count("N")
-        return num_N
+        return '{'+str(num_N)+'}'
     
     def removeN(self):
         firstN = self.find("N")
         lastN  = self.rfind("N")+1
-        print(self[:firstN]+'{'+str(self.getN())+'}'+self[lastN:])
+        print(self[:firstN]+self.getN()+self[lastN:])
     
 """
-Builds a new DNAString object based on what the user inputs
-Replaces the string of  
+Builds a new str object of SequenceCleaner class based on what the user inputs.
+Removes the ambiguous parts of the sequence, outputs the “cleaned” sequence, 
+replacing the ambiguous parts with a count in {}’s  
 """
 DNA = input("Enter a DNA sequence: ")
-seq = DNAString(DNA)
+seq = SequenceCleaner(DNA)
 seq.removeN()
