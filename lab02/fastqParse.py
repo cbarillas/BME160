8a6eba22896bcf312f9b385c49f371c8c33d1201
cbarilla@ucsc.edu <cbarilla@unix1.lt.ucsc.edu>
2017-04-16 13:02:42 -0700

    fastq now removes @ symbol and parses string

---
+++ lab02/fastqParse.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# Name: Carlos Barillas (cbarilla)
+# Group Members: none
+
+class SequenceCleaner(str):
+    """
+    SequenceCleaner class returns a string object in upper case letters.
+    Keyword arguments:
+    sequence -- DNA sequence user enters
+    """
+    def __new__(self, sequence):
+        """Returns a copy of sequence in upper case letters."""
+        return str.__new__(self, sequence.upper())
+
+    def parseString(self):
+        """
+        First removes '@' symbol 
+        Then splits string based on ':' delimiter
+        """
+        newString = list()
+        newString = self[1:]
+        parsed = newString.split(":")
+        instr,rid,fid,fcl,tnum,x,y = parsed
+        print(parsed)
+    
+"""
+Builds a new string object of SequenceCleaner class based on what the user 
+inputs.
+Removes the ambiguous parts of the sequence, outputs the “cleaned” 
+sequence, replacing the ambiguous parts with a count in {}’s  
+"""
+DNA = input("Enter a DNA sequence: ")
+seq = SequenceCleaner(DNA)
+seq.parseString()
