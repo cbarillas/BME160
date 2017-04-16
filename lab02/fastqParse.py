1bdb07a6c5b64430cce05a4d33409f452bc124b1
cbarilla@ucsc.edu <cbarilla@unix1.lt.ucsc.edu>
2017-04-16 16:53:31 -0700

    modified new method, made code have less lines

--- lab02/fastqParse.py
+++ lab02/fastqParse.py
@@ -2,34 +2,29 @@
 # Name: Carlos Barillas (cbarilla)
 # Group Members: none
 
-class SequenceCleaner(str):
+class FastQParse(str):
     """
     SequenceCleaner class returns a string object in upper case letters.
     Keyword arguments:
     sequence -- DNA sequence user enters
     """
     def __new__(self, sequence):
-        """Returns a copy of sequence in upper case letters."""
-        return str.__new__(self, sequence.upper())
+        """Returns a copy of sequence without '@'."""
+        return str.__new__(self, sequence[1:])
 
     def parseSequence(self):
-        """
-        First removes '@' symbol 
-        Then splits sequence based on ':' delimiter
-        """
-        newSeq = list()
-        newSeq = self[1:]
-        parsedSeq = newSeq.split(":")
-        instr,rid,fid,fcl,tnum,x,y = parsedSeq
-        print('Instrument = '+instr+'\nRun ID = '+rid+'\nFlow Cell ID = '+fid)
-        print('Flow Cell Lane = '+fcl+'\nTile Number = '+tnum)
-        print('X-coord = '+x+'\nY-coord = '+y)
+        """Splits string based on ':' delimiter. Prints out info."""
+        parsedSeq = self.split(":")
+        instr, rid, fcid, fcl, tnum, x, y = parsedSeq
+        print('Instrument = '+instr+'\nRun ID = '+rid+
+              '\nFlow Cell ID = '+fcid+'\nFlow Cell Lane = '+fcl+
+              '\nTile Number = '+tnum+'\nX-coord ='+x+'\nY-coord = '+y)
 """
 Builds a new string object of SequenceCleaner class based on what the user 
 inputs.
 Removes the ambiguous parts of the sequence, outputs the “cleaned” 
 sequence, replacing the ambiguous parts with a count in {}’s  
 """
-seqname = input("Enter a single line of a FASTQ formatted file: ")
-seq = SequenceCleaner(seqname)
+seqName = input("Enter a single line of a FASTQ formatted file: ")
+seq = FastQParse(seqName)
 seq.parseSequence()
