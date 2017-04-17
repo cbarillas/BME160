6b7eae821b0d78a9fa4212e868b47fc01acf5eba
cbarilla@ucsc.edu <cbarilla@unix2.lt.ucsc.edu>
2017-04-17 16:47:26 -0700

    done with coordMathSoln

--- lab02/coordinateMathSoln.py
+++ lab02/coordinateMathSoln.py
@@ -31,11 +31,11 @@ class Triad:
     # Private helper methods.
     def d2(self, a, b):
         """Calculate squared distance of point a to b."""
-        return float(sum((ia-ib)*(ia-ib) for ia,ib in zip (a,b))) 
+        return float(sum((ia-ib)*(ia-ib) for ia, ib in zip (a, b))) 
 
     def ndot(self, a, b, c):
         """Dot Product of vector a, c standardized to b."""
-        return float(sum((ia-ib)*(ic-ib) for ia,ib,ic in zip (a,b,c)))
+        return float(sum((ia-ib)*(ic-ib) for ia, ib, ic in zip (a, b, c)))
 
     # Calculate lengths(distances) of segments PQ and QR.
     def dPQ(self):
@@ -54,13 +54,13 @@ class Triad:
         sqrt(self.d2(self.p, self.q)*self.d2(self.r, self.q))) * 180/pi
 
 coordinates = input("Enter three sets of atomic coordinates: \n")
-leftP = coordinates.replace( '(', ',')
+leftP = coordinates.replace('(', ',')
 rightP = leftP.replace (')', ',')
 myList = rightP.split (',')
 
-p = (float(myList[1]),float(myList[2]), float(myList[3]))
-q = (float(myList[5]),float(myList[6]), float(myList[7]))
-r = (float(myList[9]),float(myList[10]), float(myList[11]))
+p = (float(myList[1]), float(myList[2]), float(myList[3]))
+q = (float(myList[5]), float(myList[6]), float(myList[7]))
+r = (float(myList[9]), float(myList[10]), float(myList[11]))
 
 triad = Triad(p, q, r)
 
