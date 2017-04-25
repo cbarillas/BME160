370bc78282df5c996c1f3c2bedb619789c8d15e2
cbarilla@ucsc.edu <cbarilla@unix3.lt.ucsc.edu>
2017-04-25 11:35:34 -0700

    added docstrings

--- lab03/proteinParams.py
+++ lab03/proteinParams.py
@@ -4,16 +4,18 @@
 
 
 class ProteinParam(str):
-    # These tables are for calculating:
-    #     molecular weight (aa2mw), along with the mol. weight of H2O (mwH2O)
-    #     absorbance at 280 nm (aa2abs280)
-    #     pKa of positively charged Amino Acids (aa2chargePos)
-    #     pKa of negatively charged Amino acids (aa2chargeNeg)
-    #     and the constants aaNterm and aaCterm for pKa of the respective termini
-    #  Feel free to move these to appropriate methods as you like
-
-    # As written, these are accessed as class attributes, for example:
-    # ProteinParam.aa2mw['A'] or ProteinParam.mwH2O
+    """Creates a ProteinParam string object in upper case letters. 
+    
+    Attributes:
+        attr1 (dict): Dictionary of molecular weights of amino acids.
+        attr2 (float): Molecular weight of H20.
+        attr3 (dict): Dictionary of absorbance values at 280nm.
+        attr4 (dict): Dictionary of positively charged amino acids.
+        attr5 (dict): Dictionary of negatively charged amino acids.
+        attr6 (float): Charge of the N terminus.
+        attr7 (float): Charge of the C terminus.
+        attr8 (dict): Dictionary of valid aa's.
+    """
 
     aa2mw = {
         'A': 89.093, 'G': 75.067, 'M': 149.211, 'S': 105.093, 'C': 121.158,
@@ -31,84 +33,118 @@ class ProteinParam(str):
     aaCterm = 2.34
     aaDictionary = {}
 
-
-    # the __init__ method requires a protein string to be provided, either as a
-    # string or list of strings that will be concatenated
     def __init__(self, protein):
+        """Takes in sequence from users input then creates a list of 
+        strings and concatnates to create a string object. 
+        Then creates aaDictionary with the keys being the valid aa's of 
+        users input and the value of each one being their corresponding count.
+
+        Args:
+            param1 (str): String of amino acids. 
+        """
         myList = ''.join(protein).split()
+        print(myList)
         self.proteinString = ''.join(myList).upper()
         
-        for aa in self.aa2mw.keys():  # Iterates through the aa2mw's keys (valid aa's)
-            self.aaDictionary[aa] = self.protString.count(aa)  # Each key has a count as value
+        for aa in self.aa2mw.keys(): 
+            self.aaDictionary[aa] = float(self.proteinString.count(aa))
 
     def aaCount(self):
         """ Iterates through every character in string and returns a count of valid 
         amino acids. Ignores invalid characters.
+        
         Returns:
-             aaTotal (int): Total number of valid amino acids.  
+            aaTotal (int): Total number of valid amino acids.  
         """
         aaTotal = 0
-        for aa in self.protString:                          # Iterates through each character in string object.
-            if aa.upper() in self.aa2mw.keys():  # Checks if character is valid by checking if it's in dictionary aa2mw.
+        for aa in self.proteinString:
+            if aa.upper() in self.aa2mw.keys():
                 aaTotal += 1
         return aaTotal
 
-
     def pI(self):
-        maxCharge = 10000
+        """Estimates the theoretical isoelectric point by finding the 
+        particular pH that yeilds a neutral net charge (as close to 
+        zero as possible, accurate to two decimal places).
+
+        Returns:
+            (float): Best pH.
+        """
+        bigCharge = 2**11
         bestPH = 0
-        PH = 0
-        while PH < 14.01:
-            charge = self.charge(PH)
-            if charge < maxCharge:
-                maxCharge = charge
-                bestPH = PH
-            PH += 0.01
+        particularPH = 0
+        while particularPH < 14.01:
+            charge = self.charge(particularPH)
+            if charge < bigCharge:
+                bigCharge = charge
+                bestPH = particularPH
+            particularPH += 0.01  # Needs to iterate through every pH in decimal places.
         return bestPH
 
-
     def aaComposition(self):
-        """ Constructs dictionary consisting of amino acids as keys and the count of each one as values.
-        Returns: A dictionary keyed by single letter amino acid code, having 
-        associated values that are the counts of those amino acids in the sequence.
+        """ 
+        Returns: 
+            aaDictionary created in __init__ method.
         """
         return self.aaDictionary
 
-
     def charge(self, pH):
-
+        """Calculates the net charge on the protein at specific pH using pKa of each
+        charged amino acid, Nterminus and Cterminus.
+        Args:
+            param1 (float): pH value from pI method.
+        Returns:
+            (float): Net charge of the protein.
+        """
         posCharge = 0
         for aa in self.aa2chargePos:
             nAA = self.aaDictionary[aa]
-            posCharge += nAA *((10**self.aa2chargePos[aa])/(10**self.aa2chargePos[aa]+10**pH))
-        posCharge += (10**self.aaNterm)/(10**self.aaNterm + 10**pH)
+            posCharge += nAA * ((10**self.aa2chargePos[aa]) /
+            (10**self.aa2chargePos[aa] + 10**pH))
+        posCharge += (10**self.aaNterm) / (10**self.aaNterm + 10**pH)
 
         negCharge = 0
         for aa in self.aa2chargeNeg:
             nAA = self.aaDictionary[aa]
-            negCharge += nAA * ((10**pH)/(10**self.aa2chargeNeg[aa]+10**pH))
-        negCharge += (10**pH)/(10**self.aaCterm + 10**pH)
+            negCharge += nAA * ((10**pH) /
+            (10**self.aa2chargeNeg[aa] + 10**pH))
+        negCharge += (10**pH) / (10**self.aaCterm + 10**pH)
 
         netCharge = abs(posCharge - negCharge)
 
         return netCharge
 
     def molarExtinction(self):
+        """Estimates the molar extinction coefficient based on the number
+        of tyrosines, tryptophans, cysteines and their extinction 
+        coefficient at 280nm which can be found in dictionary(aa2abs280).
+
+        Returns:
+            (float): Molar extinction coefficient.
+        """
         tyrosine = self.aaDictionary['Y'] * self.aa2abs280['Y']
         tryptophans = self.aaDictionary['W'] * self.aa2abs280['W']
         cysteines = self.aaDictionary['C'] * self.aa2abs280['C']
         molarEx = tyrosine + tryptophans + cysteines
         return molarEx
 
-
     def massExtinction(self):
+        """Computes mass extinction by dividing molar extinction
+        by the molecular weight of corresponding protein.
+
+        Returns:
+            (float): Mass extinction value.
+        """
         myMW = self.molecularWeight()
         return self.molarExtinction() / myMW if myMW else 0.0
 
-
     def molecularWeight(self):
-        """
-        Calculates the MW of the protein sequence. 
+        """Calculates the molecular weight of the protein sequence by summing
+        the weights of the individual Amino acids and excluding the waters 
+        that are released with peptide bond formation.
+
+        Returns:
+            (float): Molecular weight.
         """
         aaWeight = 0
         waterMW = self.mwH2O * (self.aaCount()-1)
@@ -133,4 +169,4 @@ for inString in sys.stdin:
     keys.sort()
     if myAAnumber == 0: myAAnumber = 1  # handles the case where no AA are present
     for key in keys:
-        print("\t{} = {:.2%}".format(key,float(myAAcomposition[key])/myAAnumber))
+        print("\t{} = {:.2%}".format(key, myAAcomposition[key]/myAAnumber))
