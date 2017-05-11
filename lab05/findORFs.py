3b832df0e68408757bb295e31181869aed040a16
Carlos Barillas <cbarillas561@gmail.com>
2017-05-11 15:54:06 -0700

    added findORFs file to lab5 dir

---
+++ lab05/findORFs.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+
+
+########################################################################
+# CommandLine
+########################################################################
+class CommandLine():
+    '''
+    Handle the command line, usage and help requests.
+
+    CommandLine uses argparse, now standard in 2.7 and beyond. 
+    it implements a standard command line argument parser with various argument options,
+    a standard usage and help, and an error termination mechanism do-usage_and_die.
+
+    attributes:
+    all arguments received from the commandline using .add_argument will be
+    avalable within the .args attribute of object instantiated from CommandLine.
+    For example, if myCommandLine is an object of the class, and requiredbool was
+    set as an option using add_argument, then myCommandLine.args.requiredbool will
+    name that option.
+
+    '''
+
+    def __init__(self, inOpts=None):
+        '''
+        CommandLine constructor.
+        Implements a parser to interpret the command line argv string using argparse.
+        '''
+
+        import argparse
+        self.parser = argparse.ArgumentParser(
+            description='Program prolog - a brief description of what this thing does',
+            epilog='Program epilog - some other stuff you feel compelled to say',
+            add_help=True,  # default is True
+            prefix_chars='-',
+            usage='%(prog)s [options] -option1[default] <input >output'
+            )
+        self.parser.add_argument('inFile', action='store', help='input file name')
+        self.parser.add_argument('outFile', action='store', help='output file name')
+        self.parser.add_argument('-lG', '--longestGene', action='store', nargs='?', const=True, default=True,
+                                 help='longest Gene in an ORF')
+        self.parser.add_argument('-mG', '--minGene', type=int, choices=range(0, 2000), action='store',
+                                 help='minimum Gene length')
+        self.parser.add_argument('-s', '--start', action='append', nargs='?',
+                                 help='start Codon')  # allows multiple list options
+        self.parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.1')
+        if inOpts is None:
+            self.args = self.parser.parse_args()
+        else:
+            self.args = self.parser.parse_args(inOpts)
+
+
+class OrfFinder():
+    def __init__(self, seq):
+        self.seq = seg
+
+    def findOrfs(self):
+        ''' 
+        find Orfs on top strand and return list of Orfs
+
+        remember to handle the dangling start and stop cases
+
+        '''
+
+    pass
+
+    def findRevOrfs(self):
+        ''' 
+        find Orfs on the bottom strand and return that list of Orfs
+
+        remember to fixup the orfList so that it r	efers to top strand coordinates and the rev frames
+
+        '''
+        pass
+
+    def saveOrf(self, start, stop, length, frame):
+        pass
+
+
+########################################################################
+# Main
+# Here is the main program
+#
+#
+########################################################################
+
+
+def main(myCommandLine=None):
+    '''
+    Implements the Usage exception handler that can be raised from anywhere in process.  
+
+    '''
+    if myCommandLine is None:
+        myCommandLine = CommandLine(['tass2.fa',
+                                     'tass2ORFdata-ATG-100.txt',
+                                     '--longestGene',
+                                     '--start=ATG',
+                                     '--minGene=100'])
+    else:
+        myCommandLine = CommandLine(myCommandLine)
+        ###### replace the code between comments.
+        # myCommandLine.args.inFile has the input file name
+        # myCommandLine.args.outFile has the output file name
+        # myCommandLine.args.longestGene is True if only the longest Gene is desired
+        # myCommandLine.args.start is a list of start codons
+        # myCommandLine.args.minGene is the minimum Gene length to include
+        #
+        import sequenceAnalysis
+        myReader = sequenceAnalysis.fastaReader(myCommandLine.args.inFile)
+
+
+#######
+
+if __name__ == "__main__":
+    main()
+
