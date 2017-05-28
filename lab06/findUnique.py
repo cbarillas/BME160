ea644b99a226c3ee7babaf424cee7a7e16f28754
cbarilla@ucsc.edu <cbarilla@unix3.lt.ucsc.edu>
2017-05-28 16:05:24 -0700

    wrote a substring method

--- lab06/findUnique.py
+++ lab06/findUnique.py
@@ -1,16 +1,41 @@
 #!/usr/bin/env python3
 # Carlos Barillas (cbarilla)
 # Group: none
+# compile: $ python3 findUnique.py <tass2.fa 
 
 import sequenceAnalysis as SA
+from itertools import chain, combinations
 
 class findUnique:
-
-    def __init__(self):
+    def __init__(self, tRNA):
         """
-        Instantiates two objects. One of FastAreader class and one of NucParams.
-        FastAreader object reads fasta file and NucParams does the counting of the
-        sequences inside fasta file.
         """
-        self.myReader = SA.FastAreader()
+        self.tRNA = tRNA
+        self.trnaList = []
         
+    def substring(self, iterable):
+        s = tuple(iterable)
+        seen = set()
+        for size in range(1, len(s)+1):
+            for index in range(len(s)+1-size):
+                slc = iterable[index:index+size]
+                if slc not in seen:
+                    seen.add(slc)
+                    yield slc
+    
+
+
+
+
+def main():
+    
+    fastaFile = SA.FastAreader('bos-tRNA-7.fa')
+    for header, sequence in fastaFile.readFasta():
+        obj = findUnique(sequence)
+        for result in obj.substring(obj.tRNA):
+            obj.trnaList.append(set(result))
+            print(obj.trnaList)
+        
+        
+if __name__ == "__main__":
+    main()
