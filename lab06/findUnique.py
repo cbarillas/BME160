76f36e09f5996a82eccdbb5dad3c8734421b1233
Carlos Barillas <cbarillas561@gmail.com>
2017-05-30 10:16:04 -0700

    moving to mac

--- lab06/findUnique.py
+++ lab06/findUnique.py
@@ -11,30 +11,56 @@ class findUnique:
         """
         """
         self.tRNA = tRNA
-        self.trnaList = []
+        #self.trnaList = []  do i need this?
         
-    def substring(self, iterable):
-        s = tuple(iterable)
+    def substring(self, sequence):
+        """
+        Generator that yields the substrings for each tRNA sequence,
+        without duplicates.
+        Ex: 'ABABC' = ['A', 'B', 'C',
+                      'AB', 'BC'
+                      'ABA', 'BAB', 'ABC',
+                      'ABAB', 'BABC',
+                      'ABABC']
+        """
         seen = set()
-        for size in range(1, len(s)+1):
-            for index in range(len(s)+1-size):
-                slc = iterable[index:index+size]
-                if slc not in seen:
-                    seen.add(slc)
-                    yield slc
+        for size in range(1, len(sequence)+1):
+            for index in range(len(sequence)+1-size):
+                substring = sequence[index:index+size]
+                if substring not in seen:
+                    seen.add(substring)
+                    yield substring
     
 
 
 
 
 def main():
-    
+    # Initalize names
     fastaFile = SA.FastAreader('bos-tRNA-7.fa')
-    for header, sequence in fastaFile.readFasta():
-        obj = findUnique(sequence)
-        for result in obj.substring(obj.tRNA):
-            obj.trnaList.append(set(result))
-            print(obj.trnaList)
+    powersetList = []  # List to store the powerSet of each tRNA sequence
+    powersetDictionary = {} # Dictionary to store the powerSet of each tRNA sequence
+
+
+    
+    for header, sequence in fastaFile.readFasta():  # Reads fasta file and yields header/sequence.
+        obj = findUnique(sequence)  # Instantiates a new object of findUnique class.
+        powerSet = set()
+        print(header)
+        for substring in obj.substring(obj.tRNA):  # Gets substring of each tRNA sequence.
+            powerSet.add(substring)  # Adds substring to powerSet set.
+            powersetDictionary[sequence] = powerSet # key = tRNA sequence, value = their powerset.
+        powersetList.append(powerSet)  # Adds each powerSet of each tRNA sequence to a list.
+        
+    uniques = []
+    for i in range(len(powersetList)):
+        compList =  powersetList[0:i]+powersetList[i+1:-1]
+        comp = set.union(*compList)
+        tRNAps = powersetList[i]
+        uniques.append(set.difference(tRNAps, comp))
+    for i in uniques:
+         print(i)
+    
         
         
 if __name__ == "__main__":
